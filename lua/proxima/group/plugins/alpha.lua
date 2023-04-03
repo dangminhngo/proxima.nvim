986636db4ccaa500043fa7559f9bf67675e2351a
dangminhngo <dangminhngo.dev@gmail.com>
2023-04-03 11:10:03 +0700

    fix: fix alpha highlight colors

--- lua/proxima/group/plugins/alpha.lua
+++ lua/proxima/group/plugins/alpha.lua
@@ -7,11 +7,11 @@ function M.get(c, config, opts)
   local syn = c.specs.syntax
 
   return {
-    AlphaShortcut = { fg = pal.orange },
+    AlphaShortcut = { fg = pal.pink },
     AlphaHeader = { fg = pal.accent },
     AlphaHeaderLabel = { fg = pal.orange },
-    AlphaFooter = { fg = pal.accent, style = "italic" },
-    AlphaButtons = { fg = pal.teal },
+    AlphaFooter = { fg = pal.magenta, style = "italic" },
+    AlphaButtons = { fg = pal.green },
   }
 end
 
